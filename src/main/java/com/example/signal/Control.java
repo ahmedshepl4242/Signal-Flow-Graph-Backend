65b0be984576e123a4cf9ec7c63952e4ee9dc891
Ahmed samir al saed <ahmedshepl4242.com>
2024-04-18 19:01:17 +0200

    last

---
+++ src/main/java/com/example/signal/Control.java
@@ -0,0 +1,44 @@
+package com.example.signal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@CrossOrigin("api")
+@Controller
+@RestController
+
+
+public class Control {
+
+    SignalFLow signalFlow = new SignalFLow();
+
+    @PostMapping("/find-loops-paths")
+    public void findLoopsPaths(@RequestBody ArrayList<ArrayList<ArrayList<Integer>>> graph) {
+//        signalFlow.init(graph);
+        signalFlow.findPathsGains();
+
+    }
+
+//    @GetMapping("/loops")
+//    public ArrayList<ArrayList<Integer>> getLoops() {
+//        return signalFlow.findLoopsGains(0,1);
+//    }
+
+    //    @GetMapping("/paths")
+//    public ArrayList<ArrayList<Integer>> getPaths() {
+//
+//        return signalFlow.dfs(0,1);
+//    }
+    @PutMapping()
+    void put() {
+        signalFlow.findPathsGains();
+        signalFlow.combine();
+
+        signalFlow.printLoops();
+        signalFlow.computeDelta();
+
+    }
+
+}
